32a8ef6518461fec494252dc10e91659aaf7dfb1
Kevin-ecometrics <kevin@e-commetrics.com>
2023-10-28 12:52:50 -0700

    update page

--- src/app/page.js
+++ src/app/page.js
@@ -33,7 +33,6 @@ export default function Home() {
         <VideoSection />
         <AboutSection />
         <TeamSection />
-        <PortfolioSection />
         <Formulario />
         <FooterSection />
       </main>
